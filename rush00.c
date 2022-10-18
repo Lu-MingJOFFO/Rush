0332570f651f737b3cc5cd19778ef7a7c9405f1d
Lu-Ming JOFFO <95413374+Lu-MingJOFFO@users.noreply.github.com>
2022-10-18 18:03:17 +0200

    Add files via upload

---
+++ rush00.c
@@ -0,0 +1,42 @@
+int	ft_putchar(char c);
+
+void	line(char a, char b, int x)
+{
+	int	i;
+
+	ft_putchar(a);
+	if (x > 2)
+	{
+		i = 0;
+		while (i < (x - 2))
+		{
+			ft_putchar(b);
+			i = i + 1;
+		}
+	}
+	if (x > 1)
+	{
+		ft_putchar(a);
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	if (x <= 0 || y <= 0)
+		return ;
+	line('o', '-', x);
+	if (y > 2)
+	{
+		i = 0;
+		while (i < (y - 2))
+		{
+			line('|', ' ', x);
+			i++;
+		}
+	}
+	if (y > 1)
+		line('o', '-', x);
+}
---
+++ rush03.c
@@ -0,0 +1,42 @@
+int	ft_putchar(char c);
+
+void	line(char a, char b, char c, int x)
+{
+	int	i;
+
+	ft_putchar(a);
+	if (x > 2)
+	{
+		i = 0;
+		while (i < (x - 2))
+		{
+			ft_putchar(b);
+			i = i + 1;
+		}
+	}
+	if (x > 1)
+	{
+		ft_putchar(c);
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	if (x <= 0 || y <= 0)
+		return ;
+	line('A', 'B', 'C', x);
+	if (y > 2)
+	{
+		i = 0;
+		while (i < (y - 2))
+		{
+			line('B', ' ', 'B', x);
+			i++;
+		}
+	}
+	if (y > 1)
+		line('A', 'B', 'C', x);
+}
